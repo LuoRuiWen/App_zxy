c224f55e6c17c668d424e7a00b0e16c740c0cfcd
LRW <1987960694@qq.com>
2019-04-25 00:15:02 +0800

    补充提交

--- src/main/java/com/zxy/utils/AppAutoConfigure.java
+++ src/main/java/com/zxy/utils/AppAutoConfigure.java
@@ -1,4 +1,10 @@
 package com.zxy.utils;
 
-public class AppAutoConfigure {
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author lrw
+ * @date 2019-4-25
+ */
+public class AppAutoConfigure implements WebMvcConfigurer {
 }
