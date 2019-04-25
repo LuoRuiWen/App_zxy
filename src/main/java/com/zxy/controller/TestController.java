375590f227a38c6e0129a1f14b5dd71630f6a8fe
LRW <1987960694@qq.com>
2019-04-26 01:44:38 +0800

    layui和thymeleef集成

--- src/main/java/com/zxy/controller/TestController.java
+++ src/main/java/com/zxy/controller/TestController.java
@@ -1,14 +1,21 @@
 package com.zxy.controller;
 
 import org.apache.shiro.authz.annotation.RequiresGuest;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiresGuest
-@RestController
+@Controller
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
     }
+
+    @GetMapping("/")
+    public String loginPage(){
+        return "login";
+    }
+
 }
